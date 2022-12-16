f8623021bc4c5676d95406245ace5846782b656f
Keshav Sharma <keshav2743@gmail.com>
2022-12-16 19:02:58 +0530

    completed

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const content = ["It was supposed to be a dream vacation. They had planned it over a year in advance so that it would be perfect in every way. It had been what they had been looking forward to through all the turmoil and negativity around them. It had been the light at the end of both their tunnels.",
                  "She wondered if the note had reached him. She scolded herself for not handing it to him in person. She trusted her friend, but so much could happen. She waited impatiently for word",
                  "The river slowly meandered through the open space. It had hidden secrets that it didn't want to reveal. It had a well-planned strategy to appear calm, inviting, and appealing. That's how the river lured her unknowing victims to her water's edge.",
-                 "There was little doubt that the bridge was unsafe. All one had to do was look at it to know that with certainty. Yet Bob didn't see another option. He may have been able to work one out if he had a bit of time to think things through, but time was something he didn't have. A choice needed to be made, and it needed to be made quickly."
+                 "There was little doubt that the bridge was unsafe. All one had to do was look at it to know that with certainty. Yet Bob didn't see another option. He may have been able to work one out if he had a bit of time to think things through, but time was something he didn't have. A choice needed to be made, and it needed to be made quickly.",
+                 "April seriously wondered about her sleeping partner choices. She looked at her bed and what a mess it had become. How did she get to the point in her life where she had two dogs, three cats, and a raccoon sleeping with her every night?",
+                 "There was no time. He ran out of the door without half the stuff he needed for work, but it didn't matter. He was late and if he didn't make this meeting on time, someone's life may be in danger."
+
                 ];
 
 
@@ -10,8 +13,9 @@ const btn = document.getElementById('btn');
 const con = document.getElementById('container');
 const cont = document.getElementById('content');
 const timer  = document.querySelector('.timer');
-const overlap = document.querySelector('.overlap');
-let ch = document.createElement('span');
+const display = document.querySelector('.disp');
+let wrong;
+
 
 let btnmsg = 'Start';   
 
@@ -21,17 +25,20 @@ let string1 , string2;
 
 let count = 0, idx = 0, array=[];
 
+let mistakes = 0;
+let totalWords = 0, myWords =0, totalChar = 0;
+
 
 function start(){
 
 
-    console.log(typeof string1);
+   
      for(let i=0; i<string1.length; i++)
      {
         array.push(string1[i]);
      }
     
-    console.log(array);
+ 
 
 
     count = 0, idx = 0;
@@ -62,15 +69,36 @@ function start(){
 }
 
 function end(){
+    clearInterval(timerf);
+
+    etime = new Date();
+
+    let totalTime = (etime-stime)/1000;
+
+    
+    let totalCharacters = display.childElementCount - wrong.length;
+    let finalmsg = document.createElement('p');
+   
+    display.innerHTML = '';
+    display.append(finalmsg);
+    
+    let words = Math.round((totalWords/totalChar)*totalCharacters);
+
+   let speed = Math.round((words/totalTime)*60);
+ 
+   finalmsg.innerHTML = `Your speed : ${speed} wpm`;
+   
+
 
     con.value= '';
     timer.innerHTML = `00 : 60`
     
-    clearInterval(timerf);  
+  
 
-     etime = new Date();
 
-    cont.innerHTML = 'Start Now!';
+    
+
+    cont.innerHTML = 'Play Again!';
     con.innerText = '';
 
 }
@@ -78,36 +106,40 @@ function end(){
 function keyHandler(e){
     // console.log(e);
   
+    wrong = document.querySelectorAll('.wrong');
    
-    if(idx==array.length-1)
-    {
-        end();
-    }
+   
     
     if(e.inputType=='deleteContentBackward')
     {
-        console.log('backspace');
+        display.removeChild(display.lastElementChild);
+        // console.log('backspace');
         idx--;
         return;
     }
+    let ch = document.createElement('span');
    
+    ch.append(e.data);
    
     if(e.data==array[idx])
     {
        
-        ch.append(e.data);
         ch.classList.remove('wrong');
         ch.classList.add('correct');
-        overlap.append(ch);
+        
+
 
     }
     else{
-        ch.append(e.data);
+        
         ch.classList.remove('correct');
         ch.classList.add('wrong');
-        overlap.append(ch);
-        console.log('incorrect');
     }
+
+    // console.log(ch);
+    display.append(ch);
+    
+
     idx++;
 
 
@@ -118,6 +150,16 @@ function clickHandler(){
     const randIdx  = Math.floor(Math.random()*content.length);
     string1 = content[randIdx];
     cont.innerHTML = string1;
+
+    let tempString = string1.split(' ');
+
+    let charString = string1.split('');
+    totalChar = charString.filter(w => w!='').length;
+    
+    totalWords = tempString.filter(w=>w!==' ').length;
+
+   
+ 
     
     if(btn.innerText=='Start')
     {
